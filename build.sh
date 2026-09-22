db65b707aa44ea8411c992d08f67b132e8e1bfa5
Kyle-00 <kylekinyua001@gmail.com>
2026-09-22 21:19:31 +0300

    feat(build): ensure superuser exists during the build process

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ python backend/manage.py collectstatic --no-input
 echo "Applying database migrations..."
 python backend/manage.py migrate
 
+echo "Ensuring superuser exists..."
+python backend/manage.py ensure_superuser
+
 echo "Build complete."
