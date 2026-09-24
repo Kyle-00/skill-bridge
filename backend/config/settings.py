5705314daf60b6ea53c1bfe4ff02bdb16f283bb2
Kyle-00 <kylekinyua001@gmail.com>
2026-09-24 14:18:56 +0300

    feat(settings): add 'daphne' to INSTALLED_APPS for ASGI support

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS if h.strip()]
 # Applications
 # ---------------------------------------------------------------------------
 INSTALLED_APPS = [
+    'daphne',      
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
